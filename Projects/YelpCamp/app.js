aece63967dab31700c4110f9dc834e3536ae89b4
Izabela da Silva Neves <izabeladasilva10@gmail.com>
2026-05-22 21:11:19 -0300

    new/edit method put

--- Projects/YelpCamp/app.js
+++ Projects/YelpCamp/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override')
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp')
@@ -14,7 +15,7 @@ db.once('open', () => {
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -56,7 +57,27 @@ app.get('/campgrounds/:id', async (req, res) => {
     }
 });
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const campground = await Campground.findById(id);
+        res.render('campgrounds/edit', { campground });
+    } catch (err) {
+        console.log(err);
+        res.send('Error fetching campground');
+    }
+});
 
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+        res.redirect(`/campgrounds/${campground._id}`);
+    } catch (err) {
+        console.log(err);
+        res.send('Error fetching campground')
+    }
+});
 
 app.listen(3000, () => {
     console.log('Serving on port 3000');
